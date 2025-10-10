be1af513ba8b5cfb0221b14eed0e0ce6e18d5bc0
Rushikesh Dhande <161755491+rushikeshdhande@users.noreply.github.com>
2025-10-10 17:24:51 +0530

    Update program10.c

--- LB/program10.c
+++ LB/program10.c
@@ -1,91 +1,92 @@
 /*
-     step1 : understand the problem statment
-     step2 : write the algorithm
-     step3 : decide the programming language
-     step4 : write the program 
-     step5 : test the program
+     Create the project first understand the 5 step carefuliy
+    Step 1 - Understand the problem statement
+    Step 2 - write the algorithm
+    step 3 - Decide the programming language
+    Step 4 - write the program
+    Step 5 - Test the program
 */
-/*
-    Algorithm
 
+/*
+    Alogithm
     START
-          Accept first number as No1
-          Accept second number as No2
-          If the input is negative convert it into positive
-          perform Additon of No1 & No2
-          Display the Addition on screen
+        Accept First number as No1
+        Accept secnd number as No2
+        If the input is negative then convert it into positive
+        Perform Addition of No1 and No2
+        Display the Addition on Console
     STOP
 */
 
-////////////////////////////////////////////////////////
-///
-/// Required header files
-///
-////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+//////////////////////////////////////////////////////////////
 
 #include<stdio.h>
 
-////////////////////////////////////////////////////////
-//  
-//  function name: AdditionTwoNumbers
-//  discription:   It is used to perform
-//  input:         Float , Float 
-//  output:        Float 
-//  auther:        Dhananjay Ravindra Khaire 
-//  date:          09/10/2025 
+//////////////////////////////////////////////////////////////
 //
-////////////////////////////////////////////////////////
+//  Function Name   :   AdditionTwoNumbers
+//  Description     :   It is used to Perform Addition  
+//  Input           :   Float, Float             
+//  Output          :   Float
+//  Author          :   Rushikesh Baban Dhande
+//  Date            :   09/10/2025 
+//
+//////////////////////////////////////////////////////////////
 
 float AdditionTwoNumbers(float fNo1, float fNo2)
 {
-    float fsum = 0.0f;
-
-    // updator
+    float fSum = 0.0f;
+    // Updator
     if(fNo1 < 0.0f)
     {
-        fNo1 = fNo1;
+        fNo1 = -fNo1;
     }
 
     if(fNo2 < 0.0f)
     {
-        fNo2 = fNo2;
+        fNo2 = -fNo2;
     }
-
-    fsum = fNo1 + fNo2;    // business logic
-
-    return fsum;
+    
+    fSum = fNo1 + fNo2;                    //Business logic
+    
+    return fSum;
 }
 
-////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
 //
-// Entry point function for the application
+//  Entry Point Function for the Application
 //
-////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+
 int main()
-{
+{  
     float fValue1 = 0.0f, fValue2 = 0.0f, fRet = 0.0f;
 
-    printf("Enter first number : \n");
-    scanf("%f",&fValue1);
+    printf("Enter first number: \n");
+    scanf("%f", &fValue1);
 
-    printf("Enter second number : \n");
-    scanf("%f",&fValue2);
-
+    printf("Enter second number: \n");
+    scanf("%f", &fValue2);
+    
     fRet = AdditionTwoNumbers(fValue1, fValue2);
 
-    printf("Addition is : %f\n",fRet);
-
+    printf("Addition of Two Numbers is : %f \n", fRet);
 
     return 0;
 }
-////////////////////////////////////////////////////////
-//
-//  Testcases succefully handeled by the application
+
+//////////////////////////////////////////////////////////////
 //  
-//   Input : 10.5    Input : 3.2     Output : 13.7
-//   Input : -10.5   Input : 3.2     Output : 13.7
-//   Input : 10.5    Input : -3.2    Output : 13.7
-//   Input : -10.5   Input : -3.2    Output : 13.7
-//   Input : 10.5    Input : 0.0     Output : 10.5
+//  Test cases successfully handled by th application
 //
-////////////////////////////////////////////////////////
+//  Input1 : 10.5       Input2 : 3.2        Output : 13.7
+//  Input1 : -10.5      Input2 : 3.2        Output : 13.7
+//  Input1 : 10.5       Input2 : -3.2       Output : 13.7
+//  Input1 : -10.5      Input2 : -3.2       Output : 13.7
+//  Input1 : 10.5       Input2 : 0.0        Output : 10.5
+//
+//////////////////////////////////////////////////////////////
